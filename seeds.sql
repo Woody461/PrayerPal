ebcc119bc0873aeadfd7aea5ee82e6dba10929d5
Steve <stevemarti677@gmail.com>
2023-05-23 19:30:24 -0600

    fixes

--- seeds.sql
+++ seeds.sql
@@ -1,3 +1,4 @@
 
 INSERT INTO s_scriptures (verse, book, chapter, verse_number)
 VALUES ('The LORD is a refuge for the oppressed, a stronghold in times of trouble. Those who know your name will trust in you, for you, LORD, have never forsaken those who seek you. Sing praises to the LORD, enthroned in Zion; proclaim among the nations what he has done.', 'Psalms',  9, 9);
+VALUES ('The LORD is a refuge for the oppressed, a stronghold in times of trouble. Those who know your name will trust in you, for you, LORD, have never forsaken those who seek you. Sing praises to the LORD, enthroned in Zion; proclaim among the nations what he has done.', 'Psalms',  9, 9);
